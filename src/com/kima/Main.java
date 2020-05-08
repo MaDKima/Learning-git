6af599150816542adc027aa869cc10ea989cc144
kima <ramis.alexis@gmail.com>
2020-05-08 20:41:21 +0200

    first commit

---
+++ src/com/kima/Main.java
@@ -0,0 +1,8 @@
+package com.kima;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello OpenClassromms");
+    }
+}
---
+++ src/com/ocr/kima/Main.java
@@ -0,0 +1,8 @@
+package com.ocr.kima;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello OpenClassrooms");
+    }
+}
